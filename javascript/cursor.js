9f2e3cc5db4821fae880e9250b9e9cc75d16c9c6
Jared <jaredgomez0812@gmail.com>
2022-12-21 14:33:18 -0500

    new

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -1,34 +1,34 @@
 export function cursor() {
-  const cursor = document.querySelector('.cursor');
-  const cursorBorder = document.querySelector('.cursor__border');
-  const anchors = document.querySelectorAll('a');
+  const cursor = document.querySelector(".cursor");
+  const cursorBorder = document.querySelector(".cursor__border");
+  const anchors = document.querySelectorAll("a");
 
-  document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
   });
 
-  document.addEventListener('click', (e) => {
-    cursor.classList.add('click');
+  document.addEventListener("click", (e) => {
+    cursor.classList.add("click");
     setTimeout(() => {
-      cursor.classList.remove('click');
+      cursor.classList.remove("click");
     }, 200);
   });
 
   anchors.forEach((anchor) => {
-    anchor.addEventListener('mouseover', () => {
-      cursorBorder.style.display = 'none';
-      cursor.style.backgroundImage = 'none';
-      cursor.style.transform = 'scale(2)';
-      cursor.style.animationName = 'borderAnim';
-      cursor.style.mixBlendMode = 'difference';
+    anchor.addEventListener("mouseover", () => {
+      cursorBorder.style.display = "none";
+      cursor.style.backgroundImage = "none";
+      cursor.style.transform = "scale(2)";
+      cursor.style.animationName = "borderAnim";
+      cursor.style.mixBlendMode = "difference";
     });
-    anchor.addEventListener('mouseleave', () => {
-      cursorBorder.style.display = 'block';
-      cursor.style.backgroundImage = 'url(../assets/images/cursor.png)';
-      cursor.style.transform = '';
-      cursor.style.animationName = '';
-      cursor.style.mixBlendMode = '';
+    anchor.addEventListener("mouseleave", () => {
+      cursorBorder.style.display = "block";
+      cursor.style.backgroundImage = "url(../assets/images/cursor.png)";
+      cursor.style.transform = "";
+      cursor.style.animationName = "";
+      cursor.style.mixBlendMode = "";
     });
   });
 }
--- javascript/parallaxHero.js
+++ javascript/parallaxHero.js
@@ -1,10 +1,10 @@
 export function parallaxHero() {
-  const background = document.querySelector('.background')
-  const foreground = document.querySelector('.foreground');
+  const background = document.querySelector(".background");
+  const foreground = document.querySelector(".foreground");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     let value = window.scrollY;
-    background.style.top = value * 0.75 + 'px';
-    foreground.style.top = value * 0  + 'px';
-  })
+    background.style.top = value * 0.75 + "px";
+    foreground.style.top = value * 0 + "px";
+  });
 }
